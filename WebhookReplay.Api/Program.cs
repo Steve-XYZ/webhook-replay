66745196a94a4bcba974e793e55aa7027abc87f4
Stive Hernandez <hernandezsuarezstive@gmail.com>
2026-08-22 16:57:43 -0400

    Merge branch 'main' into feat/replay-edit (restore retry wiring + full test union)

--- WebhookReplay.Api/Program.cs
+++ WebhookReplay.Api/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddWebhookHealthChecks();
 builder.Services.AddHttpClient();
 
 builder.Services.AddWebhookRetention(builder.Configuration);
+builder.Services.AddReplayRetries(builder.Configuration);
 
 builder.Services.AddSingleton<NpgsqlDataSource>(sp =>
 {
